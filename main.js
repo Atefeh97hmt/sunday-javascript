3613fee2f7392c28a62f51e55da6045e62b8be29
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-03-16 18:56:05 +0330

    variables

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 console.log("hi javascript");
 console.error("this is error");
 console.warn("this is warning");
+
+var text = "hi ali"
+console.log(text);
+console.log(text.length);
+console.log(text.toUpperCase());
+
+var price = 1500;
+var price2 = "1500";
+
+var isOnline = true;
+var isOnline = false;
+
+
